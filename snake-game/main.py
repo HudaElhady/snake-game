a63ca943e54d3bd8e8fb5370da5c65d9432b2a7e
huda elhady <huda.m.elhady@gmail.com>
2025-10-06 12:48:55 +0300

    implement the snake moving

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,22 +1,34 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-
-turtles_list = []
+screen.tracer(0)
+segments = []
 
 for index in range(3):
-    turtle = Turtle(shape="square")
-    turtle.color("white")
+    segment = Turtle(shape="square")
+    segment.color("white")
+    segment.penup()
     if index > 0:
-        previous_turtle = turtles_list[-1]
-        turtle.setx(previous_turtle.xcor() - 20)
+        previous_segment = segments[-1]
+        segment.setx(previous_segment.xcor() - 20)
 
-    turtles_list.append(turtle)
+    segments.append(segment)
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
+    for index in range(len(segments)-1, 0, -1):
+        current_seg = segments[index]
+        next_seg = segments[index-1]
+        current_seg.goto(next_seg.xcor(), next_seg.ycor())
+
+    segments[0].forward(20)
 
 
 screen.exitonclick()
