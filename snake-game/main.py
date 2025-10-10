7bafc3a16ac0bcf84c2cc71ba387edae0b9ec90a
huda elhady <huda.m.elhady@gmail.com>
2025-10-10 20:23:39 +0300

    show food in random positions

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,6 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,16 +11,19 @@ screen.tracer(0)
 screen.listen()
 
 snake = Snake()
+food = Food()
 screen.onkey(fun=snake.move_up, key="Up")
 screen.onkey(fun=snake.move_down, key="Down")
 screen.onkey(fun=snake.move_left, key="Left")
 screen.onkey(fun=snake.move_right, key="Right")
 game_is_on = True
 while game_is_on:
-    # screen.update()
+    screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.move_to_random_location()
 
 
 screen.exitonclick()
